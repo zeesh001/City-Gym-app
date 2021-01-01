37314f11b6aa1c58a49e6020d31f6f3aa52537cc
Zeeshan0001 <36229615+Zeeshan0001@users.noreply.github.com>
2021-01-01 22:22:40 +0000

    2 features integrated with 100 % unit testing and half interated tests with front end working

--- src/main/kotlin/org/wit/config/DbConfig.kt
+++ src/main/kotlin/org/wit/config/DbConfig.kt
@@ -1,15 +1,24 @@
 package org.wit.config
 import org.jetbrains.exposed.sql.Database
+import java.net.URI
 
 class DbConfig{
 
-    //NOTE: you need the ?sslmode=require otherwise you get an error complaining about the ssl certificate
+   // //NOTE: you need the ?sslmode=require otherwise you get an error complaining about the ssl certificate
+//    fun getDbConnection() :Database{
+//        return Database.connect(
+//            "jdbc:postgresql://ec2-35-169-92-231.compute-1.amazonaws.com:5432/d64qcmuokr0kh4?sslmode=require",
+//            driver = "org.postgresql.Driver",
+//            user = "nlgduwsarkpsqm",
+//            password = "9331b0e4ead8a69eaaa9e14990cdc5a98e6df43d520a8155ad1e2ce17de00173")
+//    }
     fun getDbConnection() :Database{
+        val databaseURL = URI(System.getenv("DATABASE_URL"))
         return Database.connect(
-            "jdbc:postgresql://ec2-35-169-92-231.compute-1.amazonaws.com:5432/d64qcmuokr0kh4?sslmode=require",
+            "jdbc:postgresql://" + databaseURL.host + ":" + databaseURL.port + databaseURL.path + "?sslmode=require",
             driver = "org.postgresql.Driver",
-            user = "nlgduwsarkpsqm",
-            password = "9331b0e4ead8a69eaaa9e14990cdc5a98e6df43d520a8155ad1e2ce17de00173")
+            user = databaseURL.userInfo.split(":").toTypedArray()[0],
+            password = databaseURL.userInfo.split(":").toTypedArray()[1])
     }
 
 }
